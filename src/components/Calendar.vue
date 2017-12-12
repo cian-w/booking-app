7d0886b5bafb9f180743c7597775ed621266a1e0
Cian Woods <cianwoods@me.com>
2017-12-12 18:45:20 +0000

    Initial commit

---
+++ src/components/Calendar.vue
@@ -0,0 +1,39 @@
+<template>
+  <vue-event-calendar :events="demoEvents"></vue-event-calendar>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      demoEvents: [{
+        date: '2016/11/12', // Required
+        title: 'Foo' // Required
+      }, {
+        date: '2016/12/15',
+        title: 'Bar',
+        desc: 'description',
+        customClass: 'disabled highlight' // Custom classes to an calendar cell
+      }]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
