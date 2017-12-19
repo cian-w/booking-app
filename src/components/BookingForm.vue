3edd8cc7caa104137ad920a23d567ad3bf8e4496
Cian Woods <cianwoods@me.com>
2017-12-19 15:46:58 +0000

    Create booking

--- src/components/BookingForm.vue
+++ src/components/BookingForm.vue
@@ -3,7 +3,7 @@
     <center>
       <br><br>
       <h2>Book a Pitch</h2>
-      <form class="form">
+      <div class="form">
         <div class="form-group">
           <label for="exampleInputEmail1">Title</label>
           <input type="text" class="form-control" v-model="title" placeholder="U15 Training">
@@ -27,13 +27,15 @@
           <label for="exampleInputPassword1">Time</label>
           <input type="text" class="form-control" v-model="time" placeholder="14:00">
         </div>
-        <button type="submit" class="btn btn-primary">Book Now</button>
-      </form>
+        <button class="btn btn-primary" v-on:click="book">Book Now</button>
+      </div>
     </center>
   </div>
 </template>
 
 <script>
+  import endpoints from '../Endpoints.js'
+
   export default {
     name: 'BookingForm',
     data () {
@@ -42,21 +44,44 @@
         description: '',
         pitch: 'Astro',
         date: '',
-        time: ''
+        time: '',
+        userId: 1
       }
     },
 
     methods: {
       // Insert booking
       book(){
-        var bookingDate = {
+        if(localStorage.getItem("session") == 'admin'){
+          this.userId = 1;
+        } else {
+          this.userId = 2;
+        }
+
+        var bookingData = {
           title: this.title,
           description: this.description,
           pitch: this.pitch,
           date: this.date,
-          time: this.time
+          time: this.time,
+          userid: this.userId
         }
 
+        if(this.title != ''){
+          fetch(endpoints.bookingsApi + `/book`,{
+             method: 'POST',
+             headers: {
+               'Content-Type': 'application/x-www-form-urlencoded'
+             },
+             body: JSON.stringify(bookingData)
+           }).then((response) => {
+             console.log();
+             // If we get a status 200 - alert booking successful
+             if(response.status == '200'){
+               alert("Booking Successful");
+             }
+           });
+         }
 
       }
     }
@@ -73,5 +98,9 @@
 
   label {
     font-weight: bold;
+  }
+
+  .btn {
+    cursor: pointer;
   }
 </style>
